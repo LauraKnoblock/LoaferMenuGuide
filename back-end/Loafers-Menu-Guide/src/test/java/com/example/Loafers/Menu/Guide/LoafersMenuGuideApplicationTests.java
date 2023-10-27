6d6ca82cfe786ecf51e8684d0e0f9aa73af08f4a
Laura Knoblock <laura.e.knoblock@gmail.com>
2023-10-27 11:30:50 -0500

    starting to add database and CRUD

---
+++ back-end/Loafers-Menu-Guide/src/test/java/com/example/Loafers/Menu/Guide/LoafersMenuGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loafers.Menu.Guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoafersMenuGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ loafers-menu-guide/src/main/java/com/example/Loafers/Menu/Guide/LoafersMenuGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.Loafers.Menu.Guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoafersMenuGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoafersMenuGuideApplication.class, args);
+	}
+
+}
